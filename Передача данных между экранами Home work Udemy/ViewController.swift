4f85d713f947a94df105db9c15abbbb56d462bfe
ihorPrudnyk <prudnyk.iv@gmail.com>
2023-01-23 08:17:27 +0000

    Initial Commit

---
+++ Передача данных между экранами Home work Udemy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Передача данных между экранами Home work Udemy
+//
+//  Created by Ihor on 23.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
